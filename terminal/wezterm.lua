396b3d5cd14a69bb542bae75bffa045990507d8d
Zhang Junkui <364772080@qq.com>
2022-04-25 03:23:39 +0800

    terminal update

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -4,9 +4,9 @@ return {
   color_scheme = "Catppuccin",
   font = wezterm.font_with_fallback({
 	"Ubuntu Mono Ligaturized",
---	"Hack",
 	"Source Han Sans CN"
   }),
   font_size = 13,
   enable_tab_bar = false,
+  window_background_opacity = 0.9,
 }
